d400adaa90596ceaa2e16787b857bcad4adf96d2
Sai Kiran Ganesh Kumar <ganeshsaikiran7@gmail.com>
2024-10-27 16:11:06 -0600

    Multiple functions and props

--- React/296-react-props-practice/src/components/App.jsx
+++ React/296-react-props-practice/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Contacts from "../contacts";
 
 function App(props) {
   return (
@@ -7,7 +8,7 @@ function App(props) {
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
-          <img className="circle-img" src={props.url} alt="avatar_img" />
+          <Avatar url={props.url} />
         </div>
         <div className="bottom">
           <div className="info">
--- React/296-react-props-practice/src/index.jsx
+++ React/296-react-props-practice/src/index.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import contacts from "./contacts";
 
 ReactDOM.render(
   <div>
     <App
-      name="Beyonce"
-      url="https://www.bing.com/images/search?view=detailV2&ccid=2P9H9Y1F&id=C233EF9264E4CCBB4F766E27BD68CB736A9961B6&thid=OIP.2P9H9Y1FTvzaZkPKW3hY3QHaEK&mediaurl=https%3a%2f%2fwww.thedigitalfix.com%2fwp-content%2fuploads%2f2022%2f05%2ftobey-maguire-spider-verse.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.d8ff47f58d454efcda6643ca5b7858dd%3frik%3dtmGZanPLaL0nbg%26pid%3dImgRaw%26r%3d0&exph=1080&expw=1920&q=tobey+maguire&simid=608011123410810691&FORM=IRPRST&ck=566E4521FBA2D35A86836C49E5FA63D9&selectedIndex=6&itb=0"
-      phone="+12345678910"
-      email="b@beyonce.com"
+      name={contacts[0].name}
+      url={contacts[0].imgURL}
+      phone={contacts[0].phone}
+      email={contacts[0].email}
     />
     <App
-      name="Jack Bauyer"
-      url="https://www.bing.com/images/search?view=detailV2&ccid=2P9H9Y1F&id=C233EF9264E4CCBB4F766E27BD68CB736A9961B6&thid=OIP.2P9H9Y1FTvzaZkPKW3hY3QHaEK&mediaurl=https%3a%2f%2fwww.thedigitalfix.com%2fwp-content%2fuploads%2f2022%2f05%2ftobey-maguire-spider-verse.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.d8ff47f58d454efcda6643ca5b7858dd%3frik%3dtmGZanPLaL0nbg%26pid%3dImgRaw%26r%3d0&exph=1080&expw=1920&q=tobey+maguire&simid=608011123410810691&FORM=IRPRST&ck=566E4521FBA2D35A86836C49E5FA63D9&selectedIndex=6&itb=0"
-      phone="+12345678910"
-      email="b@beyonce.com"
+      name={contacts[1].name}
+      url={contacts[1].imgURL}
+      phone={contacts[1].phone}
+      email={contacts[1].email}
     />
     <App
-      name="Chris Williams"
-      url="https://www.bing.com/images/search?view=detailV2&ccid=2P9H9Y1F&id=C233EF9264E4CCBB4F766E27BD68CB736A9961B6&thid=OIP.2P9H9Y1FTvzaZkPKW3hY3QHaEK&mediaurl=https%3a%2f%2fwww.thedigitalfix.com%2fwp-content%2fuploads%2f2022%2f05%2ftobey-maguire-spider-verse.jpg&cdnurl=https%3a%2f%2fth.bing.com%2fth%2fid%2fR.d8ff47f58d454efcda6643ca5b7858dd%3frik%3dtmGZanPLaL0nbg%26pid%3dImgRaw%26r%3d0&exph=1080&expw=1920&q=tobey+maguire&simid=608011123410810691&FORM=IRPRST&ck=566E4521FBA2D35A86836C49E5FA63D9&selectedIndex=6&itb=0"
-      phone="+12345678910"
-      email="b@beyonce.com"
+      name={contacts[2].name}
+      url={contacts[2].imgURL}
+      phone={contacts[2].phone}
+      email={contacts[2].email}
     />
   </div>,
   document.getElementById("root")
